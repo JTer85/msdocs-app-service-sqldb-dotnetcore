2157e58520a8b939c90c12682d5b968e9649c21d
JTer85 <105297369+JTer85@users.noreply.github.com>
2023-08-20 17:09:06 -0500

    another one for connection string

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -8,12 +8,9 @@ builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Data Source=webappdbjl-server.database.windows.net,1433;Initial Catalog=webappdbjl-database;User ID=webappdbjl-server-admin;Password=F7N588J33PHS0IZB$")));
 builder.Services.AddStackExchangeRedisCache(options =>
 {
-options.Configuration = builder.Configuration["builder.Services.AddStackExchangeRedisCache(options =>
-{
 options.Configuration = builder.Configuration["webappdbjl-cache.redis.cache.windows.net:6380,password=hutBgmbL1N3lwxs6ad5pgbO6HCrjK2H36AzCaNWgRHs=,ssl=True,defaultDatabase=0"];
 options.InstanceName = "SampleInstance";
-});"];
-
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
