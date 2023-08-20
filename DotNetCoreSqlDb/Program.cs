a55d87c68fbac5f04864cd3959850be34a8e26ef
JTer85 <105297369+JTer85@users.noreply.github.com>
2023-08-20 16:37:19 -0500

    New Commit to get this working (1)

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -8,8 +8,7 @@ builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Data Source=webappdbjl-server.database.windows.net,1433;Initial Catalog=webappdbjl-database;User ID=webappdbjl-server-admin;Password=F7N588J33PHS0IZB$")));
 builder.Services.AddStackExchangeRedisCache(options =>
 {
-options.Configuration = builder.Configuration["webappdbjl-cache.redis.cache.windows.net:6380,password=hutBgmbL1N3lwxs6ad5pgbO6HCrjK2H36AzCaNWgRHs=,ssl=True,defaultDatabase=0
-"];
+options.Configuration = builder.Configuration["webappdbjl-cache.redis.cache.windows.net:6380,password=hutBgmbL1N3lwxs6ad5pgbO6HCrjK2H36AzCaNWgRHs=,ssl=True,defaultDatabase=0"];
 options.InstanceName = "SampleInstance";
 });
 
